97149ee545783de9f89e4dc9499cea0e25cf6e07
trackedby <trackedby1@hotmail.com>
2023-04-02 21:17:58 -0300

    first commit

---
+++ server.cjs
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/download', async (req, res) => {
+  const { videoUrl } = req.body;
+  const info = await ytdl.getInfo(videoUrl);
+  const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+  const stream = ytdl.downloadFromInfo(info, { format });
+  const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  const videoId = info.videoDetails.videoId;
+  const filename = `${videoTitle}_${videoId}.mp3`;
+  res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+  res.setHeader('Content-Type', 'audio/mpeg');
+  stream.pipe(res);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ src/Home.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+function Home() {
+  const [videoUrl, setVideoUrl] = useState('');
+  
+  const handleInputChange = (newValue) => {
+    setVideoUrl(newValue);
+  };
+  
+  const handleSubmit = () => {
+    fetch('http://localhost:3000/download', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ videoUrl })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.blob();
+    })
+    .then(blob => {
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'audio.mp3';
+      link.click();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+  
+  return (
+    <div>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        handleSubmit();
+      }}>
+        <label>
+          Video URL:
+          <input type="text" value={videoUrl} onChange={(event) => {
+            handleInputChange(event.target.value);
+          }} />
+        </label>
+        <button type="submit">Download Audio</button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
