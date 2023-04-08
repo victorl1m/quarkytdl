6b4b08749f9b880f4e302e010d949af765bd536d
Victor Silva <58794491+victorl1m@users.noreply.github.com>
2023-04-08 00:42:42 -0300

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,51 +1,68 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 function Home() {
-  const [videoUrl, setVideoUrl] = useState('');
-  
-  const handleInputChange = (newValue) => {
-    setVideoUrl(newValue);
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const handleInputChange = (URL) => {
+    setVideoUrl(URL);
   };
-  
+
+  console.log(videoUrl);
+
   const handleSubmit = () => {
-    fetch('http://localhost:3000/download', {
-      method: 'POST',
+    fetch("http://localhost:3000/download", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ videoUrl })
+      body: JSON.stringify({ videoUrl }),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.blob();
-    })
-    .then(blob => {
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'audio.mp3';
-      link.click();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        const videoTitle = blob.type.split("/")[0];
+        const filename = `${videoTitle}_${Date.now()}.mp3`;
+        a.href = url;
+        a.download = filename;
+        a.click();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
-  
+
   return (
     <div>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        handleSubmit();
-      }}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit();
+        }}
+      >
         <label>
           Video URL:
-          <input type="text" value={videoUrl} onChange={(event) => {
-            handleInputChange(event.target.value);
-          }} />
+          <input
+            type="text"
+            value={videoUrl}
+            onChange={(event) => {
+              handleInputChange(event.target.value);
+            }}
+          />
         </label>
-        <button type="submit">Download Audio</button>
+        <button
+          onClick={() => {
+            console.log("clicked");
+          }}
+          type="submit"
+        >
+          Download Video
+        </button>
       </form>
     </div>
   );
